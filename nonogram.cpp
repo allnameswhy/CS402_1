8a869f8dae101102d3617fe600723a9408bbf2ca
allnameswhy <allnameswhy@gmail.com>
2018-04-05 21:27:48 +0900

    repeated combination

--- nonogram.cpp
+++ nonogram.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <string>
+#include <cstring>
 #include <sstream>
 #include <cstdlib>
 #include <algorithm>
@@ -11,20 +12,52 @@
 #include <vector>
 #include <iterator>
 
+using namespace std;
+
 static int row, column;
+static string result;
 
 template <typename Out>
 static void _split (const string s, char delim, Out result) {
     stringstream ss(s);
     string item;
     while (getline(ss, item, delim))
-        *(result++) = atoi(item);
+        *(result++) = atoi(item.c_str());
 }
 
 static vector<int> split(const string s, char delim) {
     vector<int> tokens;
     _split(s, delim, back_inserter(tokens));
     return tokens;
+}
+
+static unsigned powu(unsigned base, unsigned exp) {
+  unsigned result = 1;
+  while (exp > 0) {
+    if (exp & 1)
+      result = result * base;
+    base = base * base;
+    exp = exp >> 1;
+  }
+  return result;
+}
+
+static vector<string> repeated_permutation(char *list, unsigned perm_len) {
+  unsigned list_len = strlen(list);
+  char *perm = (char *) calloc(perm_len+1, sizeof(char));
+  int total_num = powu(list_len, perm_len);
+  vector<string> posibilities = {};
+
+  for (int i = 0; i < total_num; i++) {
+    int n = i;
+    for (int j =0; j < perm_len; j++) {
+      perm[perm_len -j -1] = list[n %list_len];
+      n = n / list_len;
+    }
+    posibilities.push_back(string(perm));
+  }
+  free(perm);
+  return posibilities;
 }
 
 vector<int> get_block_nums(bool is_row, int line_num) {
@@ -41,39 +74,36 @@ vector<int> get_block_nums(bool is_row, int line_num) {
     return block_nums;
 }
 
+
 string solve_line(bool is_row, int line_num, string line_info) {
-    vector<int> block_nums = get_block_nums(is_row, line_num);
-    vector<int> line = split(line_info, ' ');
-    string str = "";
+  int blocks_per_line = is_row? column : row;
+  vector<int> block_nums = get_block_nums(is_row, line_num);
+  vector<int> line = split(line_info, ' ');
+  string str = "";
 
-    int position;
-    int sum = 0;
-    for (int i = 0; i < line.size(); i ++) {
-        if (line.size() == 1)
-            position = 0 ;
-        else if (i == 0)
-            position = 1;
-        else if (i == line.size()-1)
-            position = 2;
-        else
-            position = 3;
-
-        form_neighbors(line[i], position, block_nums);
-        sum += line[i];
-    }
-    form_atmost(sum);
-
-    free(line);
-    free(block_nums);
-    return str;
+  // get the sum of the colored block + in-betweens
+  int sum = 0;
+  for (auto it = line.begin(); it != line.end(); it++) {
+    sum += (*it);
+    sum += (it != (line.end() - 1));
+  }
+  if (blocks_per_line == sum) {
+   for (int i = 0; i < line.size(); i++) {
+      for (int j = 0; j < line[i];  j++) {
+        if (i < line.size() - 1)
+          str.append("& ");
+        str.append(to_string(block_nums[i + j]));
+     }
+   }
+  }
 }
 
-string form_neighbors(int n, int position, vector<int> blocks) {
-    string str = "";
 
-    switch (position) {
-        case 0:
-            int i = 0;
-            while (i+n )
-    }
+
+int main(int argc, char *argv[]) {
+  row = 0;
+  column = 0;
+  result = "";
+
+  return 0;
 }
